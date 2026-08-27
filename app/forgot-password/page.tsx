0cac27f9f6f7d697b944b8b5250d6b586e67bb21
ormoon-ll <72150728+ormoon-ll@users.noreply.github.com>
2026-08-27 11:50:02 +1000

    Add files via upload

--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -2,7 +2,7 @@
 
 import { FormEvent, useState } from "react";
 import Link from "next/link";
-import { supabase } from "../../lib/supabase";
+import { supabase } from "@/lib/supabase";
 
 export default function ForgotPasswordPage() {
   const [email, setEmail] = useState("");
